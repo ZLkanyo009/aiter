1d36f73f1eff82f479c0b6b71bf16ae98c28e48b
Yutao Xu <xytpai@foxmail.com>
2025-11-24 21:02:37 +0800

    Update test_trtllm_all_reduce_fusion.py

--- op_tests/test_trtllm_all_reduce_fusion.py
+++ op_tests/test_trtllm_all_reduce_fusion.py
@@ -4,72 +4,7 @@ import torch
 import torch.multiprocessing as mp
 import torch.distributed as dist
 import aiter
-<<<<<<< HEAD
 from aiter.test_common import checkAllclose
-=======
-
-
-envs = {
-    "HIP_VISIBLE_DEVICES": "1,2,3,4,5,6,7",
-}
-for k, v in envs.items():
-    os.environ[k] = v
-
-
-def rms_norm_forward(x: torch.Tensor, weight: torch.Tensor, eps: float):
-    input_dtype = x.dtype
-    variance = x.float().pow(2).mean(-1, keepdim=True)
-    x = x * torch.rsqrt(variance + eps)
-    x = x.to(input_dtype)
-    return weight * x
-
-
-class DistributedEnv:
-    def __init__(self, rank, world_size):
-        torch.cuda.set_device(rank)
-        dist.init_process_group(
-            backend="nccl",
-            init_method="tcp://127.0.0.1:22129",
-            rank=rank,
-            world_size=world_size,
-        )
-        self.rank = rank
-        self.world_size = world_size
-        self.group = dist.group.WORLD
-        self.trtllm_instance = aiter.TRTLLMAllreduceFusion(group=self.group)
-        self.barrier()
-
-    def __del__(self):
-        dist.destroy_process_group(self.group)
-
-    def barrier(self):
-        torch.cuda.set_device(self.rank)
-        dist.barrier(self.group)
-        torch.cuda.synchronize()
-
-    def allreduce_add_rms_native(self, allreduce_in, residual_in, rms_weight, eps):
-        dist.all_reduce(allreduce_in)
-        residual_out = allreduce_in + residual_in
-        norm_out = rms_norm_forward(residual_out, rms_weight, eps)
-        return residual_out, norm_out
-
-    def allreduce_add_rms_fused(self, allreduce_in, residual_in, rms_weight, eps):
-        residual_out = torch.empty_like(residual_in)
-        norm_out = torch.empty_like(allreduce_in)
-        aiter.trtllm_allreduce_rms(
-            self.rank,
-            self.world_size,
-            allreduce_in,
-            residual_in,
-            rms_weight,
-            residual_out,
-            norm_out,
-            eps,
-            False,
-            self.trtllm_instance.get_workspace(allreduce_in),
-        )
-        return residual_out, norm_out
->>>>>>> 917945bd (fix bugs)
 
 
 def worker(
@@ -90,7 +25,6 @@ def worker(
         with prof:
             dist_env.barrier()
             start_native = time.time()
-<<<<<<< HEAD
             ref_residual_out, ref_norm_out, ref_scale_out = (
                 dist_env.allreduce_add_rms_native(
                     local_allreduce_in.clone(),
@@ -108,15 +42,6 @@ def worker(
                 local_rms_weight,
                 eps,
                 True,
-=======
-            ref_residual_out, ref_norm_out = dist_env.allreduce_add_rms_native(
-                local_allreduce_in.clone(), local_residual_in, local_rms_weight, eps
-            )
-            dist_env.barrier()
-            start_fused = time.time()
-            residual_out, norm_out = dist_env.allreduce_add_rms_fused(
-                local_allreduce_in.clone(), local_residual_in, local_rms_weight, eps
->>>>>>> 917945bd (fix bugs)
             )
             dist_env.barrier()
             end = time.time()
@@ -189,10 +114,6 @@ def testcase(
 
 
 def main(world_size=4):
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 47498b42 (refine tests)
     num_tokens = 1
     testcase(
         world_size=world_size,
@@ -200,16 +121,8 @@ def main(world_size=4):
         hidden_dim=4096,
         dtype=torch.bfloat16,
     )
-<<<<<<< HEAD
-=======
-    # num_tokens = 1
-    # testcase(world_size=world_size, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.bfloat16)
->>>>>>> c538aece (refine code)
-=======
->>>>>>> 47498b42 (refine tests)
 
     num_tokens = 129
-<<<<<<< HEAD
     testcase(
         world_size=world_size, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.float
     )
@@ -240,18 +153,6 @@ def main(world_size=4):
     testcase(
         world_size=world_size, num_tokens=32768, hidden_dim=4096, dtype=torch.bfloat16
     )
-=======
-    testcase(world_size=4, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.float)
-    testcase(world_size=4, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.bfloat16)
-    testcase(world_size=4, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.half)
-
-    num_tokens = 128
-    testcase(world_size=4, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.float)
-    testcase(world_size=4, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.half)
-    testcase(world_size=4, num_tokens=num_tokens, hidden_dim=1024, dtype=torch.bfloat16)
-
-    testcase(world_size=4, num_tokens=32768, hidden_dim=4096, dtype=torch.bfloat16)
->>>>>>> 917945bd (fix bugs)
 
 
 if __name__ == "__main__":
