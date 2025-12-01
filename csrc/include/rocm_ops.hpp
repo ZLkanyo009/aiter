dc580d6b399446dd9c6f592654709b1b05a0405a
Yutao Xu <xytpai@foxmail.com>
2025-12-01 16:15:59 +0800

    Avoid copying the IPC buffer during cudagraph replay for `arfusion` (#8)
    * Update trtllm_all_reduce_fusion.py
    
    * Update rocm_ops.hpp
    
    * Update trtllm_all_reduce_fusion.h
    
    * Update trtllm_all_reduce_fusion.cu
    
    * bugfix for return types

--- csrc/include/rocm_ops.hpp
+++ csrc/include/rocm_ops.hpp
@@ -1388,10 +1388,16 @@ namespace py = pybind11;
           py::arg("final_output"),       \
           py::arg("final_lse") = std::nullopt);
 
-#define TRTLLM_ALL_REDUCE_FUSION_PYBIND                                        \
-    m.def("trtllm_init_ar_fusion", &trtllm::init_ar_fusion);                   \
-    m.def("trtllm_destroy_ar_fusion", &trtllm::destroy_ar_fusion);             \
-    m.def("trtllm_get_ar_fusion_handle", &trtllm::get_ar_fusion_handle);       \
-    m.def("trtllm_open_ar_fusion_handles", &trtllm::open_ar_fusion_handles);   \
-    m.def("trtllm_get_ar_fusion_workspace", &trtllm::get_ar_fusion_workspace); \
+#define TRTLLM_ALL_REDUCE_FUSION_PYBIND                                                        \
+    m.def("trtllm_init_ar_fusion", &trtllm::init_ar_fusion);                                   \
+    m.def("trtllm_destroy_ar_fusion", &trtllm::destroy_ar_fusion);                             \
+    m.def("trtllm_get_ar_fusion_barrier_handle", &trtllm::get_ar_fusion_barrier_handle);       \
+    m.def("trtllm_get_ar_fusion_data_handle", &trtllm::get_ar_fusion_data_handle);             \
+    m.def("trtllm_open_ar_fusion_barrier_handles", &trtllm::open_ar_fusion_barrier_handles);   \
+    m.def("trtllm_open_ar_fusion_data_handles", &trtllm::open_ar_fusion_data_handles);         \
+    m.def("trtllm_ar_fusion_capture", &trtllm::ar_fusion_capture);                             \
+    m.def("trtllm_ar_fusion_capture_clear", &trtllm::ar_fusion_capture_clear);                 \
+    m.def("trtllm_get_ar_fusion_captured_handles", &trtllm::get_ar_fusion_captured_handles);   \
+    m.def("trtllm_get_ar_fusion_captured_offsets", &trtllm::get_ar_fusion_captured_offsets);   \
+    m.def("trtllm_open_ar_fusion_captured_handles", &trtllm::open_ar_fusion_captured_handles); \
     m.def("trtllm_allreduce_rms", &trtllm::allreduce_rms);
