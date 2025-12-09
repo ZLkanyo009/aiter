e5bbf08072002f937160aa8d259a952f08aff677
Yutao Xu <xytpai@foxmail.com>
2025-12-09 22:22:20 +0800

    Add `qknorm+rope` fused kernel (#1590)
    * add qknorm rope fused kernel
    
    * fix typo
    
    * fix lint

--- aiter/ops/fused_mrope_rms.py
+++ aiter/ops/fused_mrope_rms.py
@@ -47,3 +47,19 @@ def fused_mrope_3d_rms_set_kv(
     k_scale: float,
     v_scale: float,
 ) -> None: ...
+
+@compile_ops("module_fused_mrope_rms")
+def fused_rope_rms(
+    qkv: Tensor,
+    qw: Tensor,
+    kw: Tensor,
+    cos_sin: Tensor,
+    positions: Tensor,
+    num_tokens: int,
+    num_heads_q: int,
+    num_heads_k: int,
+    num_heads_v: int,
+    head_size: int,
+    is_neox_style: bool,
+    eps: float,
+) -> None: ...
--- csrc/include/rocm_ops.hpp
+++ csrc/include/rocm_ops.hpp
@@ -1236,6 +1236,7 @@ namespace py = pybind11;
 #define FUSED_MROPE_RMS_PYBIND                        \
     m.def("fused_mrope_3d_rms", &fused_mrope_3d_rms); \
     m.def("fused_mrope_3d_rms_set_kv", &fused_mrope_3d_rms_set_kv);
+    m.def("fused_rope_rms", &fused_rope_rms);
 
 #define SMOOTHQUANT_PYBIND                      \
     m.def("smoothquant_fwd", &smoothquant_fwd); \
